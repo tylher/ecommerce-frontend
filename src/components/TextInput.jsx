bec2e399f9d45c4966f7bf3d576a1f344cefae98
Damola <tylher123@gmail.com>
2024-03-17 22:35:03 +0100

    create sign up page

---
+++ src/components/TextInput.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useFormContext } from "react-hook-form";
+
+const TextInput = ({ name, options, type, placeholder, label, value }) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+  return (
+    <div className="flex flex-col  w-4/5  items-center gap-2">
+      <label htmlFor={name} className="self-start">
+        {label}
+      </label>
+      <input
+        {...register(name, options)}
+        type={type}
+        name={name}
+        id={name}
+        placeholder={placeholder}
+        className="border border-gray-300 w-full px-5 py-2 outline-none rounded-3xl"
+      />
+      {errors[name] && (
+        <span className="text-red-500 block self-start">
+          {errors[name].message}
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default TextInput;
